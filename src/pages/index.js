ed99beeb8271d136bb76f05ced6057b84e30c6d8
Josue Rivera <Puppy@Chanmees-iMac.local>
2018-10-19 14:00:35 -0400

    watch video text

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Learn to design and code React apps.</h1>
+    <p>React is the most popular javascript framework. It’s component-based, similar to how you use Components in Figma in order to reuse the elements in powerful ways. </p>
+    <Link to="/page-2/">Watch the video</Link>
+  </div>
+)
+
+export default IndexPage
