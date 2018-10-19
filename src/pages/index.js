c793fddaac22ccff34d6cf8570a2ebae889a569d
Josue Rivera <Puppy@Chanmees-iMac.local>
2018-10-19 14:40:50 -0400

    basic css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Learn to design and code React apps.</h1>
-    <p>React is the most popular javascript framework. It’s component-based, similar to how you use Components in Figma in order to reuse the elements in powerful ways. </p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps.</h1>
+        <p>React is the most popular javascript framework. It’s component-based, similar to how you use Components in Figma in order to reuse the elements in powerful ways. </p>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </div>
 )
 
